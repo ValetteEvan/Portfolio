f71c9f5478de220fc827f1f180efcb4850ec9fc8
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2025-04-16 11:39:51 +0200

    add nav bar action

--- portfolio/src/app/nav-bar/nav-bar.component.ts
+++ portfolio/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+enum NavBar {
+  About = 1,
+  Project = 2,
+  Contact = 3
+}
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+
+  NavBar = NavBar;
+
+  currentSection: NavBar = NavBar.About;
+
+  ngOnInit(): void {
+    
+  }
+
+  setSection(section: NavBar): void {
+    this.currentSection = section;
+    console.log(this.currentSection);
+  }
+
+  getSection(): number {
+    return this.currentSection; 
+  }
+
+  isCurrentSection(section: NavBar): boolean {
+    return this.currentSection === section;
+  }
 
 }
